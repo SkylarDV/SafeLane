e0e284bef1fda6e7b453d2f22d1c17d881c1a5dd
SkylarDV <r1003207@student.thomasmore.be>
2025-05-13 12:10:45 +0200

    Movable vehicles

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,31 @@
 let canvas;
+let game = "prior";
+let bottomVehicleImg;
+let rightVehicleImg;
+let roadbg;
+let bottomVehicle;
+let rightVehicle;
+let leftVehicle;
+let topVehicle;
+
+function preload() {
+    roadbg = loadImage('../images/roadbg.png');
+    bottomVehicleImg = loadImage('../images/bus-down.png');
+    rightVehicleImg = loadImage('../images/bus-right.png');
+    leftVehicleImg = loadImage('../images/bus-left.png');
+    topVehicleImg = loadImage('../images/bus-up.png');
+}
 
 function setup() {
     canvas = createCanvas(windowWidth / 3, windowWidth / 3);
     positionCanvas();
     background(0);
+
+    // Create instances of Vehicle
+    bottomVehicle = new Vehicle(bottomVehicleImg, px(52), px(70), px(10), px(30));
+    rightVehicle = new Vehicle(rightVehicleImg, px(70), px(35), px(30), px(10));
+    leftVehicle = new Vehicle(leftVehicleImg, px(0), px(50), px(30), px(10));
+    topVehicle = new Vehicle(topVehicleImg, px(38), px(0), px(10), px(30));
 }
 
 function windowResized() {
@@ -33,8 +55,72 @@ function positionCanvas() {
     }
 }
 
+class Vehicle {
+    constructor(image, x, y, width, height) {
+        this.image = image;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.isDragging = false;
+    }
+
+    mousePressed() {
+        if (
+            mouseX > this.x &&
+            mouseX < this.x + this.width &&
+            mouseY > this.y &&
+            mouseY < this.y + this.height
+        ) {
+            this.isDragging = true;
+        }
+    }
+
+    mouseReleased() {
+        this.isDragging = false;
+    }
+
+    drag() {
+        if (this.isDragging) {
+            this.x = mouseX - this.width / 2;
+            this.y = mouseY - this.height / 2;
+        }
+    }
+
+    draw() {
+        image(this.image, this.x, this.y, this.width, this.height);
+    }
+}
+
+function mousePressed() {
+    bottomVehicle.mousePressed();
+    rightVehicle.mousePressed();
+    leftVehicle.mousePressed();
+    topVehicle.mousePressed();
+}
+
+function mouseReleased() {
+    bottomVehicle.mouseReleased();
+    rightVehicle.mouseReleased();
+    leftVehicle.mouseReleased();
+    topVehicle.mouseReleased();
+}
+
+function priorGame() {
+    bottomVehicle.drag();
+    rightVehicle.drag();
+    leftVehicle.drag();
+    topVehicle.drag();
+    bottomVehicle.draw();
+    rightVehicle.draw();
+    leftVehicle.draw();
+    //topVehicle.draw();
+}
+
 function draw() {
-    fill(255, 0, 0);
-    noStroke();
-    ellipse(px(15), px(30), px(5), px(5));
+    clear();
+    background(roadbg);
+    if (game == "prior") {
+        priorGame();
+    }
 }
